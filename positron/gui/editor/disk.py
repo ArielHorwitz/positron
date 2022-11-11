f9aac8d549f3e0a0108d9ad511441c425974a107
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-11-11 16:08:00 +0200

    Disk browser modal

---
+++ positron/gui/editor/disk.py
@@ -0,0 +1,166 @@
+"""Modal for testing purposes."""
+
+from loguru import logger
+from pathlib import Path
+import subprocess
+from .. import kex as kx, FONTS_DIR
+from ...util import settings
+from ...util.file import USER_DIR
+
+
+FONT = str(FONTS_DIR / settings.get("ui.font"))
+UI_FONT_SIZE = settings.get("ui.font_size")
+DEFAULT_BOOKMARKS = settings.get("project.disk_bookmarks_defaults")
+BOOKMARKS = tuple(
+    Path(b).expanduser().resolve()
+    for b in settings.get("project.disk_bookmarks")
+)
+CHAR_SIZE = kx.CoreLabel(font=FONT, font_size=UI_FONT_SIZE).get_extents(text="a")
+CHAR_WIDTH, LINE_HEIGHT = CHAR_SIZE
+TREE_TOP_PREFIX = "╚╦═ "
+TREE_CHILD_PREFIX = " ╠═══ "
+TREE_CHILD_PREFIX_LAST = " ╚═══ "
+MISSING_COLOR = "#ff0000"
+FOLDER_COLOR = "#0066ff"
+FILE_COLOR = "#00ff66"
+PARENT_COLOR = "#77aaff"
+
+
+class Disk(kx.Modal):
+    def __init__(self, session, **kwargs):
+        self.session = session
+        self._current_paths = []
+        self._current_reprs = []
+        self._create_bookmarks()
+        super().__init__(**kwargs)
+        self.set_size(hx=0.85, hy=0.8)
+        self.make_bg(kx.get_color("yellow", v=0.1))
+        title = kx.Label(text="Disk Browser")
+        title.make_bg(kx.get_color("orange", v=0.3))
+        title.set_size(y=50)
+        help_label = kx.Label(font_name=FONT, font_size=UI_FONT_SIZE, halign="left")
+        self.tree_list = kx.List(
+            item_height=LINE_HEIGHT,
+            font=FONT,
+            font_size=UI_FONT_SIZE,
+        )
+        # Assemble
+        widget_frame = kx.Anchor(anchor_y="top")
+        widget_frame.add(self.tree_list)
+        main_frame = kx.Box(orientation="vertical")
+        main_frame.add(title, widget_frame, help_label)
+        self.add(main_frame)
+        # Events
+        self._trigger_refresh_items = kx.create_trigger(self._refresh_items, timeout=.1)
+        self.bind(parent=self._on_parent)
+        self.tree_list.bind(on_invoked=self._on_invoked)
+        self.im.register("Browse back", self._browse_back, "backspace")
+        self.im.register("Browse project", self._browse_project, "home")
+        self.im.register("Browse bookmarks", self._browse_bookmarks, "^ home")
+        self.im.register("Open path", self._open_path, ["^ enter", "^ numpadenter"])
+        help_label.text = "\n".join([
+            "[u]ctrl + enter[/u] : open in a new window",
+            "        [u]home[/u] : project folder",
+            " [u]ctrl + home[/u] : bookmarks",
+            "   [u]backspace[/u] : back",
+        ])
+        help_label.set_size(y=LINE_HEIGHT * 6)
+        self._set_dir(self.session.dir_tree.root)
+
+    def _create_bookmarks(self):
+        paths = [USER_DIR]
+        reprs = [_wrap_color("BOOKMARKS", PARENT_COLOR)]
+        # User defind bookmarks
+        paths.extend(BOOKMARKS)
+        reprs.extend(_path_repr(b, name_only=False) for b in BOOKMARKS)
+        # Built-in defaults
+        if DEFAULT_BOOKMARKS:
+            paths.extend([
+                self.session.dir_tree.root,
+                Path.home(),
+                USER_DIR,
+            ])
+            reprs.extend([
+                _wrap_color("Project folder", FOLDER_COLOR),
+                _wrap_color("Home folder", FOLDER_COLOR),
+                _wrap_color("Positron config folder", FOLDER_COLOR),
+            ])
+        self._bookmark_paths = paths
+        self._bookmark_reprs = reprs
+
+    def _set_dir(self, path: Path):
+        while not path.exists() or not path.is_dir():
+            path = path.parent
+        children = self._get_children(path)
+        paths = [path.parent, *children]
+        parent_repr = f"{path}/" if path != Path("/") else str(path)
+        reprs = [_wrap_color(parent_repr, PARENT_COLOR)]
+        reprs.extend(_path_repr(c, name_only=True) for c in children)
+        self._current_paths = paths
+        self._current_reprs = reprs
+        self._trigger_refresh_items()
+
+    def _browse_bookmarks(self, *args):
+        self._current_paths = self._bookmark_paths
+        self._current_reprs = self._bookmark_reprs
+        self._trigger_refresh_items()
+
+    def _refresh_items(self, *args):
+        reprs = self._current_reprs
+        items = [f"{TREE_TOP_PREFIX}{reprs[0]}"]
+        if len(reprs) > 1:
+            items.extend(f"{TREE_CHILD_PREFIX}{r}" for r in reprs[1:-1])
+            items.append(f"{TREE_CHILD_PREFIX_LAST}{reprs[-1]}")
+        self.tree_list.items = items
+
+    def _browse_back(self, *args):
+        self._set_dir(self._current_paths[0])
+
+    def _browse_project(self, *args):
+        self._set_dir(self.session.dir_tree.root)
+
+    def _on_invoked(self, w, index: int, label: str):
+        path = self._current_paths[index]
+        logger.debug(f"Invoked {path=}")
+        if path.is_dir():
+            self._set_dir(path)
+        else:
+            self.container.load(path)
+            self.dismiss()
+
+    def _on_parent(self, w, parent):
+        super()._on_parent(w, parent)
+        if parent is None:
+            return
+        self.tree_list.set_focus()
+        self._trigger_refresh_items()
+
+    def _open_path(self, *args):
+        path = self._current_paths[self.tree_list.selection]
+        if not path.exists():
+            logger.debug(f"No such path: {path}")
+            return
+        logger.info(f"Opening path: {path}")
+        command_args = ["./run-positron.sh", str(path)]
+        proc = subprocess.Popen(command_args)
+        logger.debug(f"{proc=}")
+
+    def _get_children(self, path: Path):
+        func = self.session.dir_tree.get_children_from_disk
+        return func(path, use_file_types=False, use_path_filter=False)
+
+
+def _path_repr(p: Path, name_only: bool) -> str:
+    name = p.name if name_only else str(p)
+    if p.is_dir():
+        color = FOLDER_COLOR
+        name = f"{name}/"
+    elif p.is_file():
+        color = FILE_COLOR
+    else:
+        color = MISSING_COLOR
+    return _wrap_color(name, color)
+
+
+def _wrap_color(t, color):
+    return f"[color={color}]{t}[/color]"
--- positron/gui/editor/panel.py
+++ positron/gui/editor/panel.py
@@ -12,6 +12,7 @@ from .snippets import Snippets
 from .analysis import Analysis
 from .errors import Errors
 from .search import Search
+from .disk import Disk
 
 
 class Panel(kx.Anchor):
@@ -34,6 +35,7 @@ class Panel(kx.Anchor):
             (Analysis, "analysis", "^+ a"),
             (Errors, "errors", "^+ e"),
             (Search, "search", "^+ f"),
+            (Disk, "disk", "^ k"),
         ]
         self.modals = {}
         for modal_cls, name, hotkey in modals:
