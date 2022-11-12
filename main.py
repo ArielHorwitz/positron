72343ea28660c25ff498542710ba0bffcc728ad1
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-11-12 19:41:30 +0200

    Reorganize main

--- main.py
+++ main.py
@@ -15,34 +15,10 @@ import sys
 from docopt import docopt
 from pathlib import Path
 from positron.util import settings
+
+
 logger.add(settings.USER_DIR / "debug.log", level="DEBUG", rotation="1 MB")
 logger.add(settings.USER_DIR / "errors.log", level="WARNING", rotation="100 KB")
-
-
-def _debug_args(args: dict):
-    logger.info("Parsed arguments:")
-    for k, v in args.items():
-        if len(k) > 17:
-            logger.info(f"  {k}\n      {v}")
-        else:
-            logger.info(f"  {k:>20} :  {v}")
-
-
-def _run_positron(project_path: Path):
-    logger.info("\n".join(["\n", "="*50, "Starting up Positron...", "="*50, "\n"]))
-
-    # Late import since Kivy opens window on import
-    from positron.analyze.session import Session
-    from positron.gui.app import App
-    from positron.gui.kex import restart_script
-
-    app_session = Session(project_path)
-    app = App(app_session)
-    returncode = app.run()
-    if returncode < 0:
-        logger.info("Restarting Positron...\n\n")
-        restart_script()
-    logger.info("Quit Positron.")
 
 
 @logger.catch
@@ -78,6 +54,32 @@ def main():
 
     # Run app
     _run_positron(project_path)
+
+
+def _run_positron(project_path: Path):
+    logger.info("\n".join(["\n", "="*50, "Starting up Positron...", "="*50, "\n"]))
+
+    # Late import since Kivy opens window on import
+    from positron.analyze.session import Session
+    from positron.gui.app import App
+    from positron.gui.kex import restart_script
+
+    app_session = Session(project_path)
+    app = App(app_session)
+    returncode = app.run()
+    if returncode < 0:
+        logger.info("Restarting Positron...\n\n")
+        restart_script()
+    logger.info("Quit Positron.")
+
+
+def _debug_args(args: dict):
+    logger.info("Parsed arguments:")
+    for k, v in args.items():
+        if len(k) > 17:
+            logger.info(f"  {k}\n      {v}")
+        else:
+            logger.info(f"  {k:>20} :  {v}")
 
 
 if __name__ == "__main__":
