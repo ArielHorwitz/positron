15b7cdf160bd6b9ab5f3a6e61a0b81de413ecf24
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2022-11-18 14:34:15 +0200

    Clarify ambiguous run script output

--- run-positron.sh
+++ run-positron.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR"/venv/bin/activate"
-echo "VENV:" $VIRTUAL_ENV
+echo "POSITRON VENV:" $VIRTUAL_ENV
 
 python3 $SCRIPT_DIR"/main.py" $@
