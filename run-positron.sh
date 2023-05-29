8bdf634c6eb71cfd613f35489798dc2746d9d928
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-05-29 11:27:29 +0300

    Fix run directory

--- run-positron.sh
+++ run-positron.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR"/venv/bin/activate"
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+POSITRON_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+source $POSITRON_DIR"/venv/bin/activate"
 echo "POSITRON VENV:" $VIRTUAL_ENV
 
-python3 $SCRIPT_DIR"/main.py" $@
+python3 $POSITRON_DIR"/main.py" $@
